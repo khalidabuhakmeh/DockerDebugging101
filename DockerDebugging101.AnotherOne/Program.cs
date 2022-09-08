f16ff31ee8f84cfb094b0e3fcd7f064a28a42880
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2022-09-08 10:20:04 -0400

    Initial Commit

---
+++ DockerDebugging101.AnotherOne/Program.cs
@@ -0,0 +1,4 @@
+﻿for(var i = 0; i < 100; i++)
+{
+    Console.WriteLine("Hello, Haseeb!");
+}
---
+++ DockerDebugging101/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var numbers = Enumerable.Range(1, 100);
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
